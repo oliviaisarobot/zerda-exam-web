49b3b0cdb7740999a25e3bb0891cf765291a3ec3
Olivia Gardiner <oliv.gardiner@gmail.com>
2017-01-11 11:49:35 +0100

    Working validator

---
+++ validator.js
@@ -0,0 +1,39 @@
+'use strict';
+
+var validate = {
+  run: function(feedback, scale, email) {
+    var result = false;
+
+    function checkWords(input) {
+      var result = false;
+      var counter = 0;
+
+      var allowed = ["amazing", "awesome", "blithesome", "excellent", "fabulous", "fantastic", "favorable", "fortuitous", "great", "incredible", "ineffable", "mirthful", "outstanding", "perfect", "propitious", "remarkable", "smart", "spectacular", "splendid", "stellar", "stupendous", "super", "ultimate", "unbelievable", "wondrous"];
+
+      allowed.forEach(function(e) {
+        if (input.includes(e)) {
+          counter++;
+          if (counter >= 3) {
+            result = true;
+          }
+        }
+      });
+
+      return result;
+    }
+
+    if (checkWords(feedback) && parseInt(scale) >= 10 && email.includes("@")) {
+      result = true;
+    }
+
+    return result;
+  }
+};
+
+module.exports = validate;
+
+// console.log(validate.run('amazing awesome perfect text', 'lala', 'this@this.com')); // false because of scale
+// console.log(validate.run('amazing awesome perfect text', '3', 'thisthis.com')); // false because of email and scale
+// console.log(validate.run('amazing awesome perfect text', '13', 'thisthis.com')); // false because of email and scale
+// console.log(validate.run('perfect text', '13', 'thi@sthis.com')); // false because of feedback
+// console.log(validate.run('amazing awesome perfect text', '10', 'this@this.com')); // true
